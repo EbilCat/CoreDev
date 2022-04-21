c97e1cd394494da21b7e73a72d29d49a937e0b95
David Ho <hyewjo@dso.org.sg>
2022-04-22 01:30:01 +0800

    Created base class for auto binders

---
+++ Framework/Base/BaseDataObjectAutoBinder.cs
@@ -0,0 +1,44 @@
+﻿using CoreDev.Framework;
+using UnityEngine;
+
+
+namespace CoreDev.Framework
+{
+    public abstract class BaseDataObjectAutoBinder<T> : MonoBehaviour
+    {
+        //*====================
+        //* UNITY
+        //*====================
+        protected virtual void Awake()
+        {
+            DataObjectMasterRepository.RegisterForCreation(OnDataObjectCreated);
+            DataObjectMasterRepository.RegisterForDisposing(OnDataObjectDisposed);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            DataObjectMasterRepository.UnregisterFromCreation(OnDataObjectCreated);
+            DataObjectMasterRepository.UnregisterFromDisposing(OnDataObjectDisposed);
+        }
+
+
+        //*====================
+        //* CALLBACKS - DataObjectMasterRepository
+        //*====================
+        protected virtual void OnDataObjectCreated(IDataObject obj)
+        {
+            if (obj is T)
+            {
+                obj.BindAspect(this);
+            }
+        }
+
+        protected virtual void OnDataObjectDisposed(IDataObject obj)
+        {
+            if (obj is T)
+            {
+                obj.UnbindAspect(this);
+            }
+        }
+    }
+}
